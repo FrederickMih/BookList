805121c13d6ff1864ecbb5311121fca914844d05
FrederickMih <fredtimermih@gmail.com>
2022-01-13 00:19:54 +0100

    Add and instantiate the book class

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-// 
+// Book class: Represents a book
+
+class Book {
+  constructor(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+  }
+}
+
+// UI class: Handle UI Tasks
+class UI {
+    static displayBooks() {
+        const storedBooks
+    }
+}
+
+//  Storage Class: Handles Storage
+
+// Event: Display Books
+
+// Event: Add Book
+
+// Event: Remove a Book
