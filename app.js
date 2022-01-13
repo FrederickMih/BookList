58ef8e1aa2de982c9b683a571b3bd47e3f856f39
FrederickMih <fredtimermih@gmail.com>
2022-01-13 01:07:49 +0100

    Add displayBooks method

--- app.js
+++ app.js
@@ -10,15 +10,48 @@ class Book {
 
 // UI class: Handle UI Tasks
 class UI {
-    static displayBooks() {
-        const storedBooks
-    }
+  static displayBooks() {
+    const storedBooks = [
+      {
+        title: 'First Book',
+        author: 'Fred Mih',
+        isbn: '222334',
+      },
+      {
+        title: 'Second Book',
+        author: 'Ngum Mih',
+        isbn: '442334',
+      },
+    ];
+    const books = storedBooks;
+
+    books.forEach((book) => UI.addBookToList(book));
+  }
+  static addBookToList(book) {
+    const list = document.querySelector('#book-list');
+    const row = document.createElement('tr');
+    row.innerHTML = `
+       <td>${book.title}</td>
+        <td>${book.author}</td>
+         <td>${book.isbn}</td>
+          <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+       `;
+    list.appendChild(row);
+  }
 }
 
 //  Storage Class: Handles Storage
 
 // Event: Display Books
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 // Event: Add Book
+document.querySelector('#book-form').addEventListener('submit', (e)
+=> {
+// Get form values
+const title = document.querySelector('#title').value;
+const author = document.querySelector('#author').value;
+const isbn = document.querySelector('#isbn').value;
+});
 
 // Event: Remove a Book
