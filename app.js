226ae457b45f44fcf7c4118487ad72084a01ff2f
FrederickMih <fredtimermih@gmail.com>
2022-01-13 10:45:37 +0100

    Add alert method

--- app.js
+++ app.js
@@ -46,6 +46,16 @@ class UI {
     }
   }
 
+  static showAlert(message, className) {
+    const div = document.createElement('div');
+    div.className = `alert alert-${className}`;
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector('.container');
+    const form = document.querySelector('#book-form');
+    container.insertBefore(div, form);
+
+  }
+
   static clearFields() {
     document.querySelector('#title').value = '';
     document.querySelector('#author').value = '';
@@ -66,6 +76,12 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
   const title = document.querySelector('#title').value;
   const author = document.querySelector('#author').value;
   const isbn = document.querySelector('#isbn').value;
+
+  // Validations
+  if(title === '' || author === '' || isbn === '') {
+      UI.showAlert('Please fill in all fields', 'danger');
+  }else {
+
   // Instantiate book
   const book = new Book(title, author, isbn);
 
@@ -74,6 +90,7 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
 
   // Clear fields
   UI.clearFields();
+  }
 });
 // Event: Remove a Book
 document.querySelector('#book-list').addEventListener('click', (event) => {
