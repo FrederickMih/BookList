642fb1e44f94a04f726dbd71503db154f4b2d2e9
FrederickMih <fredtimermih@gmail.com>
2022-01-13 09:13:03 +0100

    Add methods to clear fields and delete books

--- app.js
+++ app.js
@@ -39,6 +39,18 @@ class UI {
        `;
     list.appendChild(row);
   }
+
+  static deleteBook(el) {
+      if(el.classList.contains('delete')) {
+          el.parentElement.parentElement.remove();
+      }
+  }
+
+  static clearFields() {
+      document.querySelector('#title').value = '';
+      document.querySelector('#author').value = '';
+      document.querySelector('#isbn').value = '';
+  }
 }
 
 //  Storage Class: Handles Storage
@@ -47,17 +59,25 @@ class UI {
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 // Event: Add Book
-document.querySelector('#book-form').addEventListener('submit', (e) =>
- {
-    // prevent actual submit
-    e.preventDefault();
-    // Get form values
-const title = document.querySelector('#title').value;
-const author = document.querySelector('#author').value;
-const isbn = document.querySelector('#isbn').value;
-// Instantiate book
-const book = new Book(title, author, isbn);
-console.log(book)
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+  // prevent actual submit
+  e.preventDefault();
+  // Get form values
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  const isbn = document.querySelector('#isbn').value;
+  // Instantiate book
+  const book = new Book(title, author, isbn);
+
+  // Add Book to UI
+  UI.addBookToList(book);
+
+  // Clear fields
+  UI.clearFields();
+});
+// Event: Remove a Book
+document.querySelector('#book-list').addEventListener('click', (event) => {
+  UI.deleteBook(event.target);
 });
 
-// Event: Remove a Book
+
